6054b25d1b1c2394ca4497f881acd788593deec3
Walter <walterdcarrizo.19@gmail.com>
2024-12-18 22:36:16 -0300

    feat: enhance Products module with validation and DTOs for create and update operations

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  public name: string;
+
+  @IsString()
+  public description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 8,
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
+
+  @IsNumber()
+  @Min(0)
+  @Type(() => Number)
+  public stock: number;
+}
