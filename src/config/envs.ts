93cdca0d266096bf3124e49885fa71f8bf40191d
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-01-23 14:45:42 -0300

    feat: add NATS integration for product validation and update environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,14 +4,21 @@ import * as Joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema: Joi.ObjectSchema = Joi.object({
   PORT: Joi.number().required(),
   DATABASE_URL: Joi.string().required(),
+
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -22,4 +29,6 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+
+  natsServers: envVars.NATS_SERVERS,
 };
